2d5f91ba5ded022ecc41b7a92eced05db4e6eb76
AngelPerez10 <angeelp7457@gmail.com>
2024-05-22 16:43:02 -0600

    Cambios de inicio de sesión
    Prueba 1

--- loginSignup/base/static/js/login.js
+++ loginSignup/base/static/js/login.js
@@ -1,19 +1,3 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-app.js";
-import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-auth.js";
-
-const firebaseConfig = {
-    apiKey: "AIzaSyC4Gje4nsjZt_Kt49aRc7Hbs9pSO78_4bo",
-    authDomain: "mathpou-bd-d5e06.firebaseapp.com",
-    projectId: "mathpou-bd-d5e06",
-    storageBucket: "mathpou-bd-d5e06.appspot.com",
-    messagingSenderId: "724724958779",
-    appId: "1:724724958779:web:1047b6ef2e7fc698723e1e",
-    measurementId: "G-XX775D1CTJ"
-};
-
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-
 // Función para iniciar sesión
 function loginUser(email, password) {
     if (!email || !password) {
@@ -21,36 +5,17 @@ function loginUser(email, password) {
         return;
     }
 
-    signInWithEmailAndPassword(auth, email, password)
-        .then((userCredential) => {
-            // Usuario ha iniciado sesión
-            const user = userCredential.user;
-            console.log('Usuario ha iniciado sesión con éxito:', user);
-            swal({
-                title: "Inicio de Sesión Exitoso",
-                text: "Has iniciado sesión con éxito.",
-                icon: "success",
-                button: "Aceptar",
-            }).then(() => {
-                window.location = 'index.html'; // Redirige al usuario a index.html
-            });
-        })
-        .catch((error) => {
-            const errorCode = error.code;
-            const errorMessage = error.message;
-            console.error('Error al iniciar sesión:', errorCode, errorMessage);
-            let message = "Error al iniciar sesión. Por favor, inténtalo de nuevo.";
-            
-            if (errorCode === 'auth/wrong-password') {
-                message = 'La contraseña es incorrecta.';
-            } else if (errorCode === 'auth/user-not-found') {
-                message = 'No existe un usuario con este correo electrónico.';
-            } else if (errorCode === 'auth/invalid-email') {
-                message = 'El correo electrónico es incorrecto.';
-            }
-
-            swal("Error al iniciar sesión: " + message, "", "error");
-        });
+    // Simulando un inicio de sesión exitoso
+    const user = { email: email }; // Objeto de usuario simulado
+    console.log('Usuario ha iniciado sesión con éxito:', user);
+    swal({
+        title: "Inicio de Sesión Exitoso",
+        text: "Has iniciado sesión con éxito.",
+        icon: "success",
+        button: "Aceptar",
+    }).then(() => {
+        window.location = 'index.html'; // Redirige al usuario a index.html
+    });
 }
 
 // Añade un escuchador al botón de inicio de sesión
